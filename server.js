43953730d1998f978edea285e867afb13ab415a9
barvinds <barvinds.ug.21.cb@francisxavier.ac.in>
2025-03-21 12:45:25 +0530

    Initial commit

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ 
+  origin: '*', 
+  methods: ['GET', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
